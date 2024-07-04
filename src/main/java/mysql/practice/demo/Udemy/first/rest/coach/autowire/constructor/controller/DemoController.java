3f4b5f8c57b9bc6bf764b3f3d855fb39f78aac69
akash ranjan verma <akash.ranjan.verma@netcracker.com>
2024-07-04 14:29:18 +0530

    push

--- src/main/java/mysql/practice/demo/Udemy/first/rest/coach/autowire/constructor/controller/DemoController.java
+++ src/main/java/mysql/practice/demo/Udemy/first/rest/coach/autowire/constructor/controller/DemoController.java
@@ -1,5 +1,6 @@
-package mysql.practice.demo.Udemy.first.rest.coach.autowire.constructor;
+package mysql.practice.demo.Udemy.first.rest.coach.autowire.constructor.controller;
 
+import mysql.practice.demo.Udemy.first.rest.coach.autowire.constructor.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/mysql/practice/demo/Udemy/first/rest/coach/autowire/setter/controller/DemoController2.java
+++ src/main/java/mysql/practice/demo/Udemy/first/rest/coach/autowire/setter/controller/DemoController2.java
@@ -1,5 +1,6 @@
-package mysql.practice.demo.Udemy.first.rest.coach.autowire.setter;
+package mysql.practice.demo.Udemy.first.rest.coach.autowire.setter.controller;
 
+import mysql.practice.demo.Udemy.first.rest.coach.autowire.setter.Coach2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
